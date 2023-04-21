e598cfd6d40d2a374cd86a831e82160fe85e5afe
ripp4rd0c <hououinkyoma13@protonmail.com>
2023-04-21 14:55:12 +0200

    fixed GET method

--- methods.js
+++ methods.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const urlPath = require('./utils/urlPath');
-
+const mime = require('mime')
 const methods = Object.create(null);
 
 // GET: returns list of files if reading dir, else file contents
 methods.GET = async (req) => {
-    const path = urlPath(req.path);
+    const path = urlPath(req.url);
+    console.log("fetching ", path);
     let stats;
     try{
         stats = await fs.promises.stat(path);
